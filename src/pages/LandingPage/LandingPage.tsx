6c31e4387f585f23a4c26fc97223aebdd3c6b378
GabryelKadmo <gabryelkadmo@gmail.com>
2025-04-30 22:15:34 -0300

    fix: corrigi o caminho do open graph e troquei a imagem da landing page

--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import UnderConstruction from "./images/PaginaEmConstrucao.webp";
+import UnderConstruction from "./images/UnderConstruction.webp";
 
 export default function page() {
   return (
