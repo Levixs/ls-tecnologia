5d3331ee725985b5a4f0509e368bd5df36ccdbfe
GabryelKadmo <gabryelkadmo@gmail.com>
2025-05-01 23:40:15 -0300

    fix: remover componente Dick e suas imagens do projeto

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,7 +1,6 @@
 import LandingPage from "@/pages/LandingPage/LandingPage";
 import { Route, Routes } from "react-router-dom";
 import NotFound from "../pages/NotFound/NotFound";
-import Dick from "@/pages/dick/dick";
 
 export function Router() {
   return (
@@ -9,8 +8,6 @@ export function Router() {
       <Routes>
         <Route path="*" element={<NotFound />} />
         <Route path="/" element={<LandingPage />} />
-        <Route path="/pinto" element={<Dick />} />
-        {/* Add more routes here as needed */}
       </Routes>
     </>
   );
